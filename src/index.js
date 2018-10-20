71b7176e96ead9e55eb6fe801153a90516cb4cdf
颜海镜 <yanhaijing@yeah.net>
2018-10-20 22:20:17 +0800

    feat: 完成代码

--- src/index.js
+++ src/index.js
@@ -1,14 +1,44 @@
-import { yan } from './test.js';
+import { extend } from '@jsmini/extend';
+import { Guid } from '@jsmini/guid';
 
-import yan2 from './test.js';
+const g = new Guid();
+const doc = document;
+const docCharset = doc.charset;
+const head = doc.head || doc.getElementsByTagName('head')[0] || doc.documentElement;
 
-console.log(yan);
-console.log(yan2);
+// export function loadimg(src, success, error, option) {
 
-var a = 1 + 1;
-var b = a;
-console.log(a);
-console.log(b);
+// }
 
-export const name = 'base';
+// export function loadcss(src, success, error, option) {
 
+// }
+
+export function loadjs(src, success, error, option) {
+    const d = extend({
+        charset: docCharset,
+        cache: true
+    }, option);
+
+    const guid = g.guid();
+
+    if (!d.cache) {
+        src += '?t=' + guid;
+    }
+    const node = doc.createElement('script');
+    node.src = src;
+    node.id = 'loadjs-js-' + guid;
+    node.charset = d.charset;
+    if ('onload' in node) {
+        node.onload = success;
+        node.onerror = error;
+    } else {
+        node.onreadystatechange = function() {
+            if (/loaded|complete/.test(node.readyState)) {
+                success();
+            }
+        };
+    }
+
+    head.appendChild(node);
+}
