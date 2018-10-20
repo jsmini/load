55143e903aa676d873633be0c0e9aaaa7e221e2a
颜海镜 <yanhaijing@yeah.net>
2018-10-20 22:24:27 +0800

    feat: update

--- test/test.js
+++ test/test.js
@@ -7,13 +7,7 @@ describe('单元测试', function() {
 
     describe('功能1', function() {
         it('相等', function() {
-            expect(base.name).to.equal('base');
-        });
-    });
-
-    describe('功能2', function() {
-        it('不相等', function() {
-            expect(base.name).not.to.equal(1);
+            expect('base').to.equal('base');
         });
     });
 });
